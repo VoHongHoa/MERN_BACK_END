1f8d9e634b944426f9eba828913db4015e4e4950
Tran Van Thien <tranvanthienuit@gmail.com>
2022-04-13 08:24:25 +0700

    file

--- models/Product.js
+++ models/Product.js
@@ -3,28 +3,36 @@ const Schema = mongoose.Schema;
 
 const ProductSchema = new Schema(
   {
-    title: {
-      type: String,
-      required: true,
-      unique: true,
-    },
-    desc: { type: String, required: true },
-    img: {
-      type: Buffer,
-      required: true,
-    },
-    categories: {
-      type: String,
-    },
+      title: {
+          type: String,
+          required: true,
+          unique: true,
+      },
+      desc: {type: String, required: true},
+      img: {
+          type: Buffer,
+          required: true,
+      },
+      categories: {
+          type: String,
+      },
 
-    color: {
-      type: String,
-      required: true,
-    },
-    price: {
-      type: Number,
-      required: true,
-    },
+      color: {
+          type: String,
+          required: true,
+      },
+      price: {
+          type: Number,
+          required: true,
+      },
+      ram: {
+          type: String,
+          required: true
+      },
+      rom: {
+          type: String,
+          required: true
+      }
   },
   { timestamps: true }
 );
